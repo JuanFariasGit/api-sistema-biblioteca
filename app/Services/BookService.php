ada5b92b5d3b0529bf8569274b07394353adfd74
Juan <juanfarias580@gmail.com>
2025-06-20 19:18:21 -0300

    feat: adicionando service BookService

---
+++ app/Services/BookService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Book;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class BookService extends Service
+{
+    protected function model(): Book
+    {   
+        return app(Book::class);
+    }
+
+    public function all(): LengthAwarePaginator
+    {
+        return $this->model()::paginate(5);
+    }
+
+    public function findById(string $id): ?Book
+    {
+        return $this->model()::find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model()::create($data);
+    }
+
+    public function update(Book $Book, $data)
+    {
+        return $Book->update($data);
+    }
+
+    public function delete(Book $Book)
+    {
+        return $Book->delete();
+    }
+} 
