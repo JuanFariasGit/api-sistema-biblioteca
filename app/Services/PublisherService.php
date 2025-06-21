e67a9869b79b1dcffce4105d04ecad31e6010f83
Juan <juanfarias580@gmail.com>
2025-06-21 08:21:44 -0300

    chore: retirando oque não esta sendo usado

--- app/Services/PublisherService.php
+++ app/Services/PublisherService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\Publisher;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 class PublisherService extends Service
 {
