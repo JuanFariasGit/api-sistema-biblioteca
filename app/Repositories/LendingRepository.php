5fc2dfa0d720c542379c91e5355de7e3808c8a27
Juan <juanfarias580@gmail.com>
2025-06-28 19:08:37 -0300

    feat:  adicionando carregamento onsioso

--- app/Repositories/LendingRepository.php
+++ app/Repositories/LendingRepository.php
@@ -3,11 +3,21 @@
 namespace App\Repositories;
 
 use App\Models\Lending;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class LendingRepository extends Repository
 {
     protected function model(): Lending
     {
         return app(Lending::class);
+    }
+
+    public function paginate(int $perPage = 10): LengthAwarePaginator
+    {
+        return $this->model()::with(
+            [
+                    'book',
+                    'reader'
+                ])->paginate($perPage);
     }
 }
