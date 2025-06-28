9289bff5454957888ef4ace3fe3514eb09c8e20a
Juan <juanfarias580@gmail.com>
2025-06-28 12:00:06 -0300

    feat: retornando os books

--- app/Repositories/ReaderRepository.php
+++ app/Repositories/ReaderRepository.php
@@ -3,11 +3,20 @@
 namespace App\Repositories;
 
 use App\Models\Reader;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ReaderRepository extends Repository
 {
     protected function model(): Reader
     {   
         return app(Reader::class);
+    }
+
+    public function paginate(int $perPage = 10): LengthAwarePaginator
+    {
+        return $this->model()::with(
+            [
+                    'books'
+                ])->paginate($perPage);
     }
 } 
