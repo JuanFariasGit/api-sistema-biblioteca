ac33a43c21047149ad9e4a0d23ccb7696a69acc0
Juan <juanfarias580@gmail.com>
2025-07-01 13:24:02 -0300

    feat:  adicionando tratamento de erros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,51 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => '403 Acesso Não Autorizado!'
+                ], 403);
+            }
+        });
+        
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => '404 Não Encontrado!'
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => '405 Método Não Encontrado!'
+                ], 405);
+            }
+        });
+
+         $exceptions->render(function (ParseError $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => '500 Erro Interno do Servidor!'
+                ], 500);
+            }
+        });
     })->create();
