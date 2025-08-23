2fa57daf2b5eb21d4c8007af9dc89dc51ebdb698
juan <juanfarias580@gmail.com>
2025-08-23 10:52:18 -0300

    feat: adicionando testes da feature publisher

---
+++ tests/Feature/Publisher/CreateTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+
+it('only logged in users can create publishers', function () {
+    $data = [
+        'name' => 'Publisher 1',
+    ];
+
+    $this->postJson('/api/publishers', $data)
+        ->assertStatus(401);
+});
+
+it('should be able to create a publisher', function () {
+    $user = User::factory()->create();
+
+    $accessToken = $this->postJson('/api/auth/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ])->json()['access_token'];
+
+    $data = [
+        'name' => 'Publisher 1',
+    ];
+
+    $this->withToken($accessToken)->postJson('/api/publishers', $data)
+        ->assertStatus(201);
+});
+
+it('name should be required', function () {
+    $user = User::factory()->create();
+
+    $accessToken = $this->postJson('/api/auth/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ])->json()['access_token'];
+
+    $this->withToken($accessToken)->postJson('/api/publishers', [])
+        ->assertJsonValidationErrorFor('name');
+});
