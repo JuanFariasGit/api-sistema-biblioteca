abc67de10c26863ac57e0c7394ebe6e8adb7f508
juan <juanfarias580@gmail.com>
2025-09-03 10:10:15 -0300

    test: adicionando testes da feature publisher

--- tests/Feature/Publisher/ListTest.php
+++ tests/Feature/Publisher/ListTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Models\{
     Publisher,
     User
@@ -36,16 +35,47 @@ it('should be a pagination', function () {
                     'created_at',
                     'updated_at',
                     'deleted_at',
+                    'books'
                 ]
             ],
+            'links' => [],
             'meta' => [
                 'total',
                 'last_page',
                 'per_page',
+                'links',
                 'current_page',
                 'from',
-                'to'
-            ]
+                'to',
+                'path'
+            ],
         ])
         ->assertStatus(200);
+});
+
+it('the user should only view their publishers', function() {
+    $userA = User::factory()->create();
+    
+    $accessTokenA = $this->postJson('/api/auth/login', [
+        'email' => $userA->email,
+        'password' => 'password',
+    ])->json()['access_token'];
+
+    Publisher::factory()
+        ->count(10)
+        ->for($userA)
+        ->create();
+    
+    $responseA = $this->withToken($accessTokenA)->getJson('/api/publishers');
+    
+    $userB = User::factory()->create();
+
+    $accessTokenB = $this->postJson('/api/auth/login', [
+        'email' => $userB->email,
+        'password' => 'password',
+    ])->json()['access_token'];
+    
+    $responseB = $this->withToken($accessTokenB)->getJson('/api/publishers');
+    
+    $this->assertNotEquals($responseA['data'], $responseB['data']);
 });
